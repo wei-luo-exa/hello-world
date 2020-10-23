93a1644650e4f0f1f3901dbaf7a9d911e09ec4c8
Wei Luo <wei@exabeam.com>
2020-10-23 13:46:07 -0700

    test

---
+++ cut_branch.sh
@@ -0,0 +1,26 @@
+set -x
+set -e
+
+BASE_BRANCH=$1
+NEW_BRANCH=$2
+
+git checkout $BASE_BRANCH
+git pull
+git checkout -b $NEW_BRANCH
+
+if [[ $NEW_BRANCH = I* ]]
+then
+    echo $NEW_BRACH > martini/upgrade_build_info.txt
+    echo $NEW_BRACH > soar-server/upgrade_build_info.txt
+    git add martini/upgrade_build_info.txt
+    git add soar-server/upgrade_build_info.txt
+elif [[ $NEW_BRANCH = PLT* ]]
+then
+    echo $NEW_BRACH > web-common/upgrade_build_info.txt
+    git add web-common/upgrade_build_info.txt
+fi
+
+git commmit -m "BLD-711 create $NEW_BRANC from $BASE_BRANCH" .
+git push
+
+
