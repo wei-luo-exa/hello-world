e34b3d9699ed505925fb48aa7713088668bb6618
Wei Luo <wei@exabeam.com>
2020-10-23 13:55:24 -0700

    BLD-711 create I98.9 from master

--- cut_branch.sh
+++ cut_branch.sh
@@ -10,13 +10,13 @@ git checkout -b $NEW_BRANCH
 
 if [[ $NEW_BRANCH = I* ]]
 then
-    echo $NEW_BRACH > martini/upgrade_build_info.txt
-    echo $NEW_BRACH > soar-server/upgrade_build_info.txt
+    echo $NEW_BRANCH > martini/upgrade_build_info.txt
+    echo $NEW_BRANCH > soar-server/upgrade_build_info.txt
     git add martini/upgrade_build_info.txt
     git add soar-server/upgrade_build_info.txt
 elif [[ $NEW_BRANCH = PLT* ]]
 then
-    echo $NEW_BRACH > web-common/upgrade_build_info.txt
+    echo $NEW_BRANCH > web-common/upgrade_build_info.txt
     git add web-common/upgrade_build_info.txt
 fi
 
